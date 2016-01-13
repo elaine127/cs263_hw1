c90a2be1940f8619f980fd3adae3b3627045cd65
elaineyang <elaineyang127@gmail.com>
2016-01-12 17:03:25 -0800

    hw1_requirement meet

---
+++ src/main/java/com/example/appengine/helloworld/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.example.appengine.helloworld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class HelloServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    PrintWriter out = resp.getWriter();
+    out.println("Hello, world");
+  }
+}
